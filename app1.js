955336fd438e6fdc622fe6c95c6d64fc7f5556be
Petar223 <49696077+Petar223@users.noreply.github.com>
2019-06-27 14:00:56 +0200

    Add files via upload

--- app1.js
+++ app1.js
@@ -15,8 +15,9 @@ var dataController = (function () {
         page: '&page='
     };
 
-    return {
 
+
+    return {
         getAjaxUrl: function () {
             return ajaxUrl;
         },
@@ -46,7 +47,8 @@ var UIController = (function () {
         h3Class: 'h1',
         imgClass: 'img',
         spanID: '#span',
-        btnClassName: 'btn'
+        btnClassName: 'btn',
+
     };
 
     //HTML input tags 
@@ -57,10 +59,11 @@ var UIController = (function () {
 
     return {
 
-        //=============================== function for creating a movie posters
-        createMovies: function (obj, imgUrl) {
+        //=============================== function for creating posters
+        createPosters: function (obj, imgUrl, gander) {
 
             for (let i = 0; i < obj.results.length; i++) {
+
                 var box = document.createElement('div');
                 box.className = DOMStrings.boxClass;
                 var imgBox = document.createElement('div');
@@ -84,10 +87,17 @@ var UIController = (function () {
                 var btn = document.createElement('button');
                 btn.className = DOMStrings.btnClassName;
 
-                var poster = imgUrl + obj.results[i].poster_path;
-                var title = obj.results[i].original_title;
-                var vote = obj.results[i].vote_average;
-                var release = obj.results[i].release_date;
+                if (gander === "movie") {
+                    var poster = imgUrl + obj.results[i].poster_path;
+                    var title = obj.results[i].original_title;
+                    var vote = obj.results[i].vote_average;
+                    var release = obj.results[i].release_date;
+                } else if (gander === "tv") {
+                    var poster = imgUrl + obj.results[i].poster_path;
+                    var title = obj.results[i].name;
+                    var vote = obj.results[i].vote_average;
+                    var release = obj.results[i].first_air_date;
+                }
 
                 img.src = !obj.results[i].poster_path ? 'no-image.jpeg' : poster;
 
@@ -105,7 +115,7 @@ var UIController = (function () {
                 content.append(h4, p, h3, p1, h31, btn)
             };
         },
-        //=============== 
+        //=============== function for show a "load more" button if total pages greater than 1
         loadBtn: function () {
             span.textContent = '';
             var loadSpan = document.createElement('div');
@@ -116,11 +126,11 @@ var UIController = (function () {
             loadSpan.append(btn);
             btn.textContent = "LOAD MORE...";
         },
-        //========================
+        //======================== function for deleting "load more" button if total pages equal to 1
         deleteBtn: function () {
             span.textContent = '';
         },
-        //===================
+        //=================== Function for restarting a search
         resetSearch: function () {
             pageCount = 1;
             container.innerHTML = '';
@@ -172,8 +182,15 @@ var controller = (function (UICtrl, dataCtrl) {
                 data.onreadystatechange = function () {
                     if (data.readyState == 4 && this.status == 200) {
                         var objData = JSON.parse(data.responseText);
-                        UICtrl.resetSearch();
-                        UICtrl.createMovies(objData, imagePtah);
+
+                        if (UICtrl.getInput().value == "movie") {
+                            UICtrl.resetSearch();
+                            UICtrl.createPosters(objData, imagePtah, "movie");
+                        } else if (UICtrl.getInput().value == "tv") {
+                            UICtrl.resetSearch();
+                            UICtrl.createPosters(objData, imagePtah, "tv");
+                        }
+
                         if (objData.total_pages > dataCtrl.getAjaxUrl().pageCount)
                             UICtrl.loadBtn();
                         else if (objData.total_pages = 1)
